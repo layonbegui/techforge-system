002bde84a66b7f5923f28eb6dec8230fd5b1de28
layonbegui <lordelayonbegui@hotmail.com>
2026-04-15 19:42:54 -0300

    usa backend online direto

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://techforge-backend-j3bu.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
